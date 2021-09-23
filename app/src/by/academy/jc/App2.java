727fae4b87b9169fa5ccf41285961ec00958eb86
AlexBy10 <90843856+AlexBy10@users.noreply.github.com>
2021-09-23 11:16:34 +0200

    Deleted tests (#6)
    Co-authored-by: Alexey Shelkovich <alexey@shelkovich.com>

--- app/src/by/academy/jc/App2.java
+++
@@ -1,7 +0,0 @@
-package by.academy.jc;
-
-public class App2 {
-    public static void main(String[] args) {
-        System.out.println("test 2");
-    }
-}
--- app/src/by/academy/jc/App4.java
+++
@@ -1,7 +0,0 @@
-package by.academy.jc;
-
-public class App4 {
-    public static void main(String[] args) {
-        System.out.println("App 4");
-    }
-}
