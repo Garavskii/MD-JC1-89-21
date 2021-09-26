65ee00f9e4d7df41fbc7f149b8fb1d3c31a3f70f
PetrashkevichOleg <petrashkevich.oleg@mail.ru>
2021-09-26 19:05:57 +0300

    test

--- app/src/by/academy/jc/petrashkevich/ht1/hometask1.java
+++ app/src/by/academy/jc/petrashkevich/ht1/hometask1.java
@@ -1,16 +1,37 @@
 package by.academy.jc.petrashkevich.ht1;
-
+import java.util.Scanner;
 public class hometask1 {
-  public static class Welcom {// creating new class(создание второго класса)
-    public Welcom(){ // creating new construktor ( создание нового конструктора)
-      System.out.println("Hi! I am constructor of second class");
+ //int x,y;
+  public static class Welcome {// creating new class(создание второго класса)
+    public Welcome(){ // creating new construktor ( создание нового конструктора)
+      System.out.println("Hi! I am constructor!");
+    }
+    public static int result(int x,int y) { // метод вычисления суммы произвеедения и суммы введенных величин
+       int z= (x+y)+x*y;
+      return (z);
+    }
+    public static void replace (int x,int y) { // метод перемены мест
+      x = x + y;
+      y = x - y;
+      x = x - y;
+      System.out.println(" The replacement was successful!");
+      System.out.println("First integr: "+x+"  Second integr: "+y);
+     //return ;
     }
   public static void main(String[] args) {
-    System.out.println("Congratulations! You are in JAVA-paradise");
-      Welcom s1 = new Welcom();
-  }
+    Scanner in=new Scanner (System.in);
+    System.out.println("Congratulations! You are in JAVA-paradise!");
+     /* Welcom s1 = */ new Welcome();// вызов конструктора приветсвия
+   System.out.print("Input first integr:  "); // ввод первой переменной с терминала
+     int x= in.nextInt(  );
+    System.out.print("Input second integr: ");// ввод второй переменной с терминала
+    int y=in.nextInt();
+    //int z = result(x, y);// вызов метода расчета суммы произведения и суммы
+
+    System.out.println("Result: " + result(x, y));
+    replace(x,y); // вызов метода замены местами переменных
+    }
   }
 }
 
 
- //
