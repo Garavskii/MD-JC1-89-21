255417fc81bf30b813efeaaed605ba64eeaef49a
PetrashkevichOleg <petrashkevich.oleg@mail.ru>
2021-09-26 10:29:59 +0300

    test

---
+++ app/src/by/academy/jc/petrashkevich/ht1/hometask1.java
@@ -0,0 +1,8 @@
+package by.academy.jc.petrashkevich.ht1;
+
+public class hometask1 {
+
+  public static void main(String[] args) {
+    System.out.println("Congratulations! You are in JAVA-paradise");
+  }
+}
