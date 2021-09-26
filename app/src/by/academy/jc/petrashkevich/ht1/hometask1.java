481c0ccbc8eca4bd777264962624684b7b1bda29
PetrashkevichOleg <petrashkevich.oleg@mail.ru>
2021-09-26 12:26:37 +0300

    test

--- app/src/by/academy/jc/petrashkevich/ht1/hometask1.java
+++ app/src/by/academy/jc/petrashkevich/ht1/hometask1.java
@@ -1,13 +1,13 @@
 package by.academy.jc.petrashkevich.ht1;
 
 public class hometask1 {
-  public static class Secondclass {// creating new class(создание нового класса)
-    public Secondclass(){ // creating new construktor ( создание нового конструктора)
+  public static class Welcom {// creating new class(создание второго класса)
+    public Welcom(){ // creating new construktor ( создание нового конструктора)
       System.out.println("Hi! I am constructor of second class");
     }
   public static void main(String[] args) {
     System.out.println("Congratulations! You are in JAVA-paradise");
-      Secondclass s1 = new Secondclass();
+      Welcom s1 = new Welcom();
   }
   }
 }
